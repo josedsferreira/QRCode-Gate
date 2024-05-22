7c5e93e06031a3a446b47f602843fe8edeeb6aec
Jose Ferreira <josedsferreira@gmail.com>
2024-05-22 17:44:05 +0100

    testes com sensor, motor esta funcional

--- gate.py
+++ gate.py
@@ -4,38 +4,30 @@ from time import time
 from time import sleep
 import RPi.GPIO as GPIO
 
-# duty cycle, calibrate if needed
-MIN_DUTY = 5
-MAX_DUTY = 10
-
+min_position = 1
+max_position = 13
+step = 1 # tem de ser inteiro
 servo_signal_pin = 13
-
-def deg_to_duty(deg):
-    return (deg - 0) * (MAX_DUTY- MIN_DUTY) / 180 + MIN_DUTY
-    
 
 def openGate():
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-
     GPIO.setup(servo_signal_pin, GPIO.OUT)
-    # set pwm signal to 50Hz
+    # pwm signal 50Hz
     servo = GPIO.PWM(servo_signal_pin, 50)
-    print("Opening Gate")
     servo.start(0)
 
-    # loop from 0 to 180
-    #for deg in range(181):
-    #    duty_cycle = deg_to_duty(deg)    
-    #    servo.ChangeDutyCycle(duty_cycle)
+    print("Opening Gate")
+    for i in range(min_position, max_position, step):
+        servo.ChangeDutyCycle(i)
+        sleep(0.5)
 
-    sleep(10)
+    sleep(5)
+
     print("Closing Gate")
+    for i in range(max_position, min_position, -step):
+        servo.ChangeDutyCycle(i)
+        sleep(0.5)
 
-    # loop from 180 to 0
-    for deg in range(180, -1, -1):
-        duty_cycle = deg_to_duty(deg)    
-        servo.ChangeDutyCycle(duty_cycle)
-
-    # cleanup the gpio pins
-    #GPIO.cleanup()
+    # cleanup gpio pins
+    GPIO.cleanup()
--- servoTeste.py
+++ servoTeste.py
@@ -1,14 +1,11 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-# duty cycle, calibrate if needed
-MIN_DUTY = 3
-MAX_DUTY = 10
+min_position = 1
+max_position = 13
+step = 1 # tem de ser inteiro
 
 servo_signal_pin = 13
-
-def deg_to_duty(deg):
-    return (deg - 0) * (MAX_DUTY- MIN_DUTY) / 180 + MIN_DUTY
 
 if __name__ == "__main__":
     GPIO.setwarnings(False)
@@ -17,18 +14,19 @@ if __name__ == "__main__":
     GPIO.setup(servo_signal_pin, GPIO.OUT)
     # set pwm signal to 50Hz
     servo = GPIO.PWM(servo_signal_pin, 50)
-    print("Opening Gate")
     servo.start(0)
-    sleep(2)
-    servo.stop()
-    
 
-    sleep(5)
+    print("Opening Gate")
+    for i in range(min_position, max_position, step):
+        servo.ChangeDutyCycle(i)
+        sleep(0.5)
+
+    sleep(1)
+
     print("Closing Gate")
-    servo.start(70)
-    sleep(2)
-    servo.stop()
-    
+    for i in range(max_position, min_position, -step):
+        servo.ChangeDutyCycle(i)
+        sleep(0.5)
 
     # cleanup the gpio pins
     GPIO.cleanup()
